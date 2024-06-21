7dc41f7387529ae67133258cfb43aadafef36c35
Natalie Gable <natgable@gmail.com>
2024-06-21 14:37:45 -0400

    some work to read and handle acs data

--- constants.py
+++ constants.py
@@ -8,3 +8,16 @@ UHF_34_SHP_URL = "https://raw.githubusercontent.com/nychealth/EHDP-data/producti
 CD_SHP_URL = (
     "https://github.com/nychealth/EHDP-data/raw/production/geography/nycd_22c/nycd.shp"
 )
+
+# ACS query
+# geo
+BASE_ZCTA_API_URL = "https://api.census.gov/data/{}/acs/acs5?get=NAME,{}&for=zip%20code%20tabulation%20area:{}"
+ZCTA_TO_UHF_URL = (
+    "https://github.com/nychealth/EHDP-data/raw/production/geography/zcta_to_uhf.csv"
+)
+
+# variable
+# add to this dictionary with any variables
+# that we might want to pull from the ACS API
+ACS_VARS = {"B19013_001E": "median_household_income", "B19083_001E": "gini_index"}
+TOT_POPULATION_VAR = "B01003_001E"
--- extract.py
+++ extract.py
@@ -1,7 +1,9 @@
 import json
+from typing import List
 
 import geopandas as gpd
 import pandas as pd
+import numpy as np
 
 import constants as const
 
@@ -61,5 +63,96 @@ def load_and_merge_geo_data() -> gpd.GeoDataFrame:
     return full_gdf
 
 
-def handle_acs_data():
-    pass
+class ACSDataHandler:
+    def __init__(self, acs_vars: List[str], years: List[int]):
+        """
+        Instatiate ACSDataHandler to load and clean data from the American Community Survey.
+
+        Args:
+            acs_vars (List[str]): the ACS-specific variable codes
+            years (List[int]): years to include in our query
+        """
+        self.acs_vars = acs_vars
+        self.acs_vars_names = [const.ACS_VARS[var_code] for var_code in self.acs_vars]
+        self.years = years
+        self.zcta_df = self._load_nyc_zcta()
+        self.zcta_list = ",".join([str(x) for x in list(self.zcta_df["zcta"].unique())])
+
+    @staticmethod
+    def _load_nyc_zcta() -> pd.DataFrame:
+        zcta_df = pd.read_csv(const.ZCTA_TO_UHF_URL)
+        zcta_df = zcta_df[zcta_df["uhfcode"] < 1000]
+        return zcta_df
+
+    def load_acs5_data(self) -> pd.DataFrame:
+        """
+        Using the Census Bureau API, query and load data for the given ACS Vars and years,
+        at the ZCTA (zip code tabulation area) level.
+
+        Returns:
+            pd.DataFrame: Dataframe with variable, year, and ZCTA columns
+        """
+        all_acs_dfs = []
+        for year in self.years:
+            single_query = const.BASE_ZCTA_API_URL.format(
+                year, ",".join(self.acs_vars), self.zcta_list
+            )
+            single_df = (
+                pd.read_csv(single_query)
+                .rename(
+                    columns=dict(
+                        **{"zip code tabulation area]": "zcta"}, **const.ACS_VARS
+                    )
+                )[list(const.ACS_VARS.values()) + ["zcta"]]
+                .assign(
+                    zcta=lambda df: df["zcta"].apply(lambda x: int(x.replace("]", ""))),
+                    year=year,
+                )
+            )
+            all_acs_dfs.append(single_df)
+        return pd.concat(all_acs_dfs)
+
+    def _get_most_recent_zcta_weights(self) -> pd.DataFrame:
+        zcta_pop_query = const.BASE_ZCTA_API_URL.format(
+            max(self.years), const.TOT_POPULATION_VAR, self.zcta_list
+        )
+        pop_df = (
+            pd.read_csv(zcta_pop_query)
+            .rename(
+                columns={
+                    "zip code tabulation area]": "zcta",
+                    const.TOT_POPULATION_VAR: "population",
+                }
+            )[["zcta", "population"]]
+            .assign(
+                zcta=lambda df: df["zcta"].apply(lambda x: int(x.replace("]", ""))),
+            )
+        )
+        return pop_df
+
+    def aggregate_zcta_to_uhf(self, df: pd.DataFrame) -> pd.DataFrame:
+        """
+        Add UHF column to dataframe and aggregate using weighted average
+        (most recent ZCTA populations as weights) the desired columns.
+
+        Args:
+            df (pd.DataFrame): dataframe from ACS data
+
+        Returns:
+            pd.DataFrame: aggregated from ZCTA to UHF code level
+        """
+        # need to get zcta weights
+        pop_weights_df = self._get_most_recent_zcta_weights()
+        merge_df = pd.merge(df, self.zcta_df, on="zcta").merge(
+            pop_weights_df, on="zcta"
+        )
+        merge_df["uhf_tot_pop"] = merge_df.groupby("uhfcode")["population"].transform(
+            np.sum
+        )
+        merge_df["weight"] = merge_df["population"].divide(merge_df["uhf_tot_pop"])
+        agg_df = (
+            merge_df.groupby(["uhfcode", "year"])[list(self.acs_vars_names)]
+            .sum()
+            .reset_index()
+        )
+        return agg_df
